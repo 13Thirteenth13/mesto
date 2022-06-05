e32548a15151319ec8bb722aa360a888c7231078
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2022-06-05 21:09:07 +0300

    Feat: реализован js функционал поп апа, оформлен readme

---
+++ script/popup.js
@@ -0,0 +1,31 @@
+let popUp = document.querySelector('.popup');
+let buttonClosePopUp = popUp.querySelector('.popup__close-button');
+let buttonSubmitPopUp = popUp.querySelector('.popup__submit-button');
+let popUpForm = popUp.querySelector('.popup__form');
+let popUpInputName = popUpForm.querySelector('#name');
+let popUpInputDescription = popUpForm.querySelector('#description');
+
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileDescription = profile.querySelector('.profile__description');
+let buttonEditProfile = profile.querySelector('.profile__edit-button');
+
+function PopUpOpen () {
+  popUp.classList.add('popup_opened');
+  popUpInputName.value = profileName.textContent;
+  popUpInputDescription.value = profileDescription.textContent;
+}
+
+function PopUpClose () {
+  popUp.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = popUpInputName.value;
+  profileDescription.textContent = popUpInputDescription.value;
+}
+
+buttonEditProfile.addEventListener('click', PopUpOpen);
+buttonClosePopUp.addEventListener('click', PopUpClose);
+buttonSubmitPopUp.addEventListener('submit', formSubmitHandler);
