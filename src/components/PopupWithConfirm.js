2a7aab94305a7f90d4a26cf6a0e7af997001f203
Sergey Krivenko <Krivenko.Sergei.V@gmail.com>
2022-08-26 17:30:31 +0300

    fix: PopupWithConfirm.js

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -4,31 +4,23 @@ export default class PopupWithConfirm extends Popup {
   constructor({ popupSelector }) {
     super(popupSelector);
     this._form = this._popup.querySelector('.popup__form');
-    this._setEvent = this._setEvent.bind(this);
   };
 
   handleSubmitConfirm(submitConfirm) {
     this._handleSubmit = submitConfirm;
   };
 
-  _setEvent(evt) {
-    evt.preventDefault();
-    this._handleSubmit();
-  };
-
   open() {
     super.open();
-    this._popup.addEventListener('submit', this._setEvent);
   };
 
   close() {
     super.close();
-    this._popup.removeEventListener('submit', this._setEvent);
   };
 
   setEventListeners() {
     super.setEventListeners();
-    this._form.addEventListener('click', (evt) => {
+    this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this._handleSubmit();
     });
